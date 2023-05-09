35eec259c58ce4b6deadacdb98478a8ada089f30
adrocodes <adrocodes@users.noreply.github.com>
2023-05-09 20:02:44 +1000

    add counter to show state preservation

--- app/donate/layout.tsx
+++ app/donate/layout.tsx
@@ -5,7 +5,9 @@ export default function Layout({ children, modal }: PropsWithChildren<{ modal: R
     <div>
       <h1>Donate Layout</h1>
       {children}
-      {modal}
+      <div className="m-2 p-4 border border-white">
+        {modal}
+      </div>
     </div>
   )
 }
--- app/donate/page.tsx
+++ app/donate/page.tsx
@@ -1,7 +1,10 @@
+import { Counter } from "@/modules/counter/Counter";
+
 export default function DonatePage() {
   return (
     <div>
       <h1>Donate Page</h1>
+      <Counter />
     </div>
   )
 }
